b0fb13f8b791fe0c3bfcd414a078a56eb2d17c9f
Josh Pencheon <josh.pencheon@googlemail.com>
2016-07-15 20:03:21 +0100

    Cautiously add bash-completion to `config`

--- .shrc/config.sh
+++ .shrc/config.sh
@@ -2,5 +2,7 @@
 # bare repository at ~/.cfg for managing dotfiles in home.
 alias config='git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 
-# ensure `config` is get git auto-completion:
-__git_complete config __git_main
+# ensure `config` gets git auto-completion:
+if command -v __git_complete > /dev/null; then
+  __git_complete config __git_main
+fi
