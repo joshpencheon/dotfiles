3103374145c1f069aeeecacadea451cd1aeca849
Josh Pencheon <josh.pencheon@googlemail.com>
2016-08-14 16:12:34 +0100

    Ensure bash completion is available bash 4.0+ too.

--- .shrc/bash.sh
+++ .shrc/bash.sh
@@ -4,3 +4,8 @@ source ~/.shrc/brew.sh
 if [ -f $(brew --prefix)/etc/bash_completion ]; then
   source $(brew --prefix)/etc/bash_completion
 fi
+
+# For bash 4.0+ instead:
+if [ -f $(brew --prefix)/share/bash-completion/bash_completion ]; then
+  source $(brew --prefix)/share/bash-completion/bash_completion
+fi
