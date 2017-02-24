098b11326f8634ae08b81b03d4c348c5fc56d24c
Josh Pencheon <josh.pencheon@googlemail.com>
2017-02-24 13:19:14 +0000

    fix regression setting

--- .dot/shrc/vim.sh
+++ .dot/shrc/vim.sh
@@ -1,6 +1,8 @@
 # Use NeoVim instead of vim, if it's available:
 if command -v nvim > /dev/null; then
-  export EDITOR=$nvim
+  export EDITOR=nvim
   alias vi=nvim
   alias vim=nvim
+else
+  export EDITOR=vi
 fi
