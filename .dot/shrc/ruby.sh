ecbbcd97ce12967d58a6ef6b87d30b378f597723
Josh Pencheon <josh.pencheon@googlemail.com>
2017-11-14 21:29:23 +0000

    rbenv: setup conditionally

--- .dot/shrc/ruby.sh
+++ .dot/shrc/ruby.sh
@@ -1,6 +1,8 @@
 # rbenv
 export PATH="$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init -)"
+if command -v rbenv > /dev/null; then
+  eval "$(rbenv init -)"
+fi
 
 # jRuby:
 export JRUBY_OPTS="--server -J-Xms1500m -J-Xmx1500m -J-XX:+UseConcMarkSweepGC -J-XX:-UseGCOverheadLimit -J-XX:+CMSClassUnloadingEnabled"
