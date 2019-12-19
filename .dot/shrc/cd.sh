14d49322f67a8ed096a105604f9dbb348e8f8553
Josh Pencheon <josh.pencheon@googlemail.com>
2019-12-19 19:27:17 +0000

    cdpath: include ~/work/gems too

--- .dot/shrc/cd.sh
+++ .dot/shrc/cd.sh
@@ -1,4 +1,4 @@
 # cd directly from listed paths.
 # Leading blank segment ensures local jumps match first.
 # Could add additional matches to ~/.bash_profile.local
-CDPATH=:~/work
+CDPATH=:~/work:~/work/gems
