b9c46cece8b5f3ef44fc8b1fa332ce128ffb89c9
Josh Pencheon <josh.pencheon@googlemail.com>
2021-06-17 13:54:07 +0100

    prompt: when on macOS, use an apple symbol for exitstatus

--- .dot/shrc/prompt/exit-status-prompt.sh
+++ .dot/shrc/prompt/exit-status-prompt.sh
@@ -1,8 +1,11 @@
 function __exit_status_ps1() {
-  if [ $? != 0 ]; then
-    printf "\033[38;5;124m• $(date +'%X')"
+  status=$?
+  dot="•" && [[ $OSTYPE == "darwin"* ]] && dot=""
+
+  if [ $status != 0 ]; then
+    printf "\033[38;5;124m$dot $(date +'%X')"
   else
-    printf "\033[38;5;112m• $(date +'%X')"
+    printf "\033[38;5;112m$dot $(date +'%X')"
   fi
 }
 
