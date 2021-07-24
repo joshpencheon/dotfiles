41701b5e4ea40d8c766d47a0e3d39d1eadc3b024
Josh Pencheon <josh.pencheon@googlemail.com>
2021-07-24 17:12:06 +0100

    go: add binary location to PATH

--- .dot/shrc/go.sh
+++ .dot/shrc/go.sh
@@ -1,3 +1,6 @@
+# Add Go default (linux) location to path:
+export PATH=$PATH:/usr/local/go/bin
+
 # Adds Go binaries to the path, if Go is installed.
 if command -v go > /dev/null; then
   export PATH=$(go env GOPATH)/bin:$PATH
