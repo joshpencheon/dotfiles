ffa725ec0d89e4bc8db4ffe4c6d53e692f69a44a
Josh Pencheon <josh.pencheon@googlemail.com>
2022-02-22 12:47:12 +0000

    nvim: use system clipboard

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -23,6 +23,7 @@ require('packer').startup(function(use)
   use 'tpope/vim-surround'    -- brackets etc
   use 'tpope/vim-unimpaired'  -- pairs of mappings
   use 'f-person/git-blame.nvim' -- Inline Git blame
+  use 'ojroques/vim-oscyank' -- OSC52 yank from server back to client clipboard.
   use 'numToStr/Comment.nvim' -- "gc" to comment visual regions/lines
   -- UI to select things (files, grep results, open buffers...)
   use { 'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' } }
@@ -86,7 +87,33 @@ vim.opt.listchars = {
 }
 vim.opt.list = true
 
+-- A note on clipboards: NeoVim's default clipboard provider will
+-- try and use `pbcopy` on macOS, but will eventually fall back to
+-- tmux's buffer.
+--
+-- Because we prefer the unnamedplus register, NeoVim will yank to
+-- and paste from one of these clipboard buffers by default.
+--
+-- It is already the case that tmux will use OSC52 codes to tell iTerm
+-- to try to update the client clipboard; we use a plugin to do something
+-- similar in NeoVim - emitting OSC52 codes when yanking into the unnamed
+-- register. This means things mostly 'just work' when accessing NeoVim
+-- over SSH.
 
+-- Use system (+ selection) clipboard by default:
+vim.cmd [[ set clipboard^=unnamed,unnamedplus ]]
+
+vim.cmd [[
+  let g:oscyank_silent=1
+
+  " Automatically emit the OSC52 control sequence when yanking:
+  augroup oscyank
+    autocmd!
+    autocmd TextYankPost * if v:event.operator is 'y' && v:event.regname is '' | OSCYankReg " | endif
+  augroup END
+]]
+
+-- Inline git blame (could this also be done with the signs plugin?)
 vim.g.gitblame_message_template = ' « <date>: <summary> [<sha>]'
 vim.g.gitblame_date_format = '%r'
 vim.g.gitblame_highlight_group = 'GitBlame'
