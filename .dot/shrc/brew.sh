c027d62b0318a43df2f40802977b3e42284521e3
Josh Pencheon <josh.pencheon@googlemail.com>
2022-04-03 11:05:47 +0100

    bash: don't source autocompletion twice

--- .dot/shrc/brew.sh
+++ .dot/shrc/brew.sh
@@ -5,15 +5,5 @@ brew_dir=$(brew --prefix)
 
 export PATH="$brew_dir/sbin:$brew_dir/bin:$PATH"
 
-# Bash autocompletion, via homebrew
-if [ -f $brew_dir/etc/bash_completion ]; then
-  source $brew_dir/etc/bash_completion
-fi
-
-# For bash 4.0+ instead:
-if [ -f $brew_dir/share/bash-completion/bash_completion ]; then
-  source $brew_dir/share/bash-completion/bash_completion
-fi
-
 # Only refresh brew once a day:
 export HOMEBREW_AUTO_UPDATE_SECS=$((60*60*24))
