c8f0d9c507b354a131f091956410a7b88d2e8115
Josh Pencheon <josh.pencheon@googlemail.com>
2022-04-25 18:12:45 +0100

    nvim: automatic bootstrapping
    - fix issue with Packer installing itself
    - defer config until appropriate plugins have been loaded

--- .config/nvim/lua/init/plugins.lua
+++ .config/nvim/lua/init/plugins.lua
@@ -1,23 +1,23 @@
 -- Install packer
 local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
-
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
   packer_bootstrap = vim.fn.system({
     'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path
   })
+  vim.o.runtimepath = vim.fn.stdpath('data') .. '/site/pack/*/start/*,' .. vim.o.runtimepath
 end
 
 vim.cmd [[
   augroup Packer
     autocmd!
-    autocmd BufWritePost init.lua PackerCompile
+    autocmd BufWritePost plugins.lua PackerSync
   augroup end
 ]]
 
 require('packer').startup(function(use)
   use 'wbthomason/packer.nvim' -- Package manager
-  use 'srcery-colors/srcery-vim'
-  use 'tpope/vim-fugitive' -- Git commands in nvim
+  use { 'srcery-colors/srcery-vim', config = [[require('init.colours')]] }
+  use { 'tpope/vim-fugitive', config = [[require('init.statusbar')]] } -- Git commands in nvim
   use 'tpope/vim-rhubarb' -- Fugitive-companion to interact with github
   use 'tpope/vim-repeat'      -- better '.' support
   use 'tpope/vim-sleuth'      -- automatic identation
@@ -26,23 +26,24 @@ require('packer').startup(function(use)
   use 'joshpencheon/vim-vinegar' -- better netrw
   -- auto-close brackets etc:
   use { 'windwp/nvim-autopairs', config = function() require('nvim-autopairs').setup() end }
-  use 'ojroques/vim-oscyank' -- OSC52 yank from server back to client clipboard.
+  use { 'ojroques/vim-oscyank', config = [[require('init.clipboard')]] } -- OSC52 yank from server back to client clipboard.
   use 'tversteeg/registers.nvim' -- preview of registers
   -- "gc" to comment visual regions/lines
   use {'numToStr/Comment.nvim', config = function() require('Comment').setup() end }
   -- UI to select things (files, grep results, open buffers...)
-  use { 'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' } }
+  use { 'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' }, config=[[require('init.telescope')]] }
   use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
-  use { "jose-elias-alvarez/buftabline.nvim" }
+  use { "jose-elias-alvarez/buftabline.nvim", config = [[require('init.tabbar')
+]] }
   -- Add git related info in the signs columns and popups
-  use { 'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' } }
+  use { 'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' }, config = [[require('init.git')]] }
   -- Highlight, edit, and navigate code using a fast incremental parsing library
-  use 'nvim-treesitter/nvim-treesitter'
+  use { 'nvim-treesitter/nvim-treesitter', config = [[require('init.treesitter')]] }
   -- Additional textobjects for treesitter
   use 'nvim-treesitter/nvim-treesitter-textobjects'
   use 'RRethy/nvim-treesitter-endwise' -- automatic 'end' insertion
   use 'neovim/nvim-lspconfig' -- Collection of configurations for built-in LSP client
-  use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
+  use { 'hrsh7th/nvim-cmp', config = [[require('init.completion')]] } -- Autocompletion plugin
   use 'hrsh7th/cmp-buffer'
   use 'hrsh7th/cmp-path'
   use 'hrsh7th/cmp-cmdline'
