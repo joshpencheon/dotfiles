4984b001a589e54f1e98f3a1cb45022cfd5adc9f
Josh Pencheon <josh.pencheon@googlemail.com>
2022-10-01 15:56:25 +0100

    nvim: add <Leader>o fallback when outside of git repo
    <Leader>O still exists for inside git repos if needed

--- .config/nvim/lua/init/telescope.lua
+++ .config/nvim/lua/init/telescope.lua
@@ -46,11 +46,17 @@ require('telescope').load_extension 'fzf'
 
 local builtin = require('telescope.builtin')
 
+function git_or_find_files(args)
+    if not pcall(builtin.git_files, args) then
+        builtin.find_files(args)
+    end
+end
+
 --Add leader shortcuts
 vim.keymap.set('n', '<leader>w', [[<cmd>w<CR>]], { silent = true })
 
 vim.keymap.set('n', '<leader>b', builtin.buffers, { silent = true })
-vim.keymap.set('n', '<leader>o', function() builtin.git_files({previewer = false}) end, { silent = true })
+vim.keymap.set('n', '<leader>o', git_or_find_files, { silent = true })
 vim.keymap.set('n', '<leader>O', function() builtin.find_files({previewer = false}) end, { silent = true })
 vim.keymap.set('n', '<leader>p', builtin.oldfiles, { silent = true })
 vim.keymap.set('n', '<leader>sb', builtin.current_buffer_fuzzy_find, { silent = true })
