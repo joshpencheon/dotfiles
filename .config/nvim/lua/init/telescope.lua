495843567481a82f98e039d323465abe2eff5e5b
Josh Pencheon <josh.pencheon@googlemail.com>
2022-10-12 15:29:31 +0100

    nvim: better multiselect

--- .config/nvim/lua/init/telescope.lua
+++ .config/nvim/lua/init/telescope.lua
@@ -3,23 +3,85 @@ local action_state = require('telescope.actions.state')
 
 local telescope_custom_actions = {}
 
-function telescope_custom_actions._multiopen(prompt_bufnr, open_cmd)
+-- From: https://github.com/nvim-telescope/telescope.nvim/issues/1048#issuecomment-1225975038
+function telescope_custom_actions.multiopen(prompt_bufnr, method)
+    local edit_file_cmd_map = {
+        vertical = "vsplit",
+        horizontal = "split",
+        tab = "tabedit",
+        default = "edit",
+    }
+    local edit_buf_cmd_map = {
+        vertical = "vert sbuffer",
+        horizontal = "sbuffer",
+        tab = "tab sbuffer",
+        default = "buffer",
+    }
     local picker = action_state.get_current_picker(prompt_bufnr)
-    local num_selections = #picker:get_multi_selection()
-    if not num_selections or num_selections <= 1 then
-        actions.add_selection(prompt_bufnr)
+    local multi_selection = picker:get_multi_selection()
+
+    if #multi_selection > 1 then
+        require("telescope.pickers").on_close_prompt(prompt_bufnr)
+        pcall(vim.api.nvim_set_current_win, picker.original_win_id)
+
+        for i, entry in ipairs(multi_selection) do
+            local filename, row, col
+
+            if entry.path or entry.filename then
+                filename = entry.path or entry.filename
+
+                row = entry.row or entry.lnum
+                col = vim.F.if_nil(entry.col, 1)
+            elseif not entry.bufnr then
+                local value = entry.value
+                if not value then
+                    return
+                end
+
+                if type(value) == "table" then
+                    value = entry.display
+                end
+
+                local sections = vim.split(value, ":")
+
+                filename = sections[1]
+                row = tonumber(sections[2])
+                col = tonumber(sections[3])
+            end
+
+            local entry_bufnr = entry.bufnr
+
+            if entry_bufnr then
+                if not vim.api.nvim_buf_get_option(entry_bufnr, "buflisted") then
+                    vim.api.nvim_buf_set_option(entry_bufnr, "buflisted", true)
+                end
+                local command = i == 1 and "buffer" or edit_buf_cmd_map[method]
+                pcall(vim.cmd, string.format("%s %s", command, vim.api.nvim_buf_get_name(entry_bufnr)))
+            else
+                local command = i == 1 and "edit" or edit_file_cmd_map[method]
+                if vim.api.nvim_buf_get_name(0) ~= filename or command ~= "edit" then
+                    filename = require("plenary.path"):new(vim.fn.fnameescape(filename)):normalize(vim.loop.cwd())
+                    pcall(vim.cmd, string.format("%s %s", command, filename))
+                end
+            end
+
+            if row and col then
+                pcall(vim.api.nvim_win_set_cursor, 0, { row, col - 1 })
+            end
+        end
+    else
+        actions["select_" .. method](prompt_bufnr)
     end
-    actions.send_selected_to_qflist(prompt_bufnr)
-    vim.cmd("cfdo " .. open_cmd)
 end
+
 function telescope_custom_actions.multi_selection_open_vsplit(prompt_bufnr)
-    telescope_custom_actions._multiopen(prompt_bufnr, "vsplit")
+    telescope_custom_actions.multiopen(prompt_bufnr, "vertical")
 end
 function telescope_custom_actions.multi_selection_open_split(prompt_bufnr)
-    telescope_custom_actions._multiopen(prompt_bufnr, "split")
+    telescope_custom_actions.multiopen(prompt_bufnr, "horizontal")
 end
 function telescope_custom_actions.multi_selection_open(prompt_bufnr)
-    telescope_custom_actions._multiopen(prompt_bufnr, "edit")
+    telescope_custom_actions.multiopen(prompt_bufnr, "default")
 end
 
 -- Telescope
