c5e5149c8b595f6e19afe7eced20073e197dc541
Josh Pencheon <josh.pencheon@googlemail.com>
2023-04-13 09:50:31 +0100

    zsh: preserve separate history file

--- .dot/shrc/history.sh
+++ .dot/shrc/history.sh
@@ -1,7 +1,13 @@
-# Bash History:
-export HISTCONTROL=ignoreboth
-export HISTFILESIZE=
-export HISTSIZE=
-export HISTTIMEFORMAT="[%F %T] "
-export HISTFILE=~/.bash_eternal_history
-export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
+if [ -n "$ZSH_VERSION" ]; then
+  export HISTFILE=~/.zsh_external_history
+  export HISTSIZE=999999999
+  export SAVEHIST=$HISTSIZE
+  setopt HIST_IGNORE_SPACE
+elif [ -n "$BASH_VERSION" ]; then
+  export HISTCONTROL=ignoreboth
+  export HISTFILESIZE=
+  export HISTSIZE=
+  export HISTTIMEFORMAT="[%F %T] "
+  export HISTFILE=~/.bash_eternal_history
+  export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
+fi
