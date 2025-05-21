a82975d1bf5794847e64b220a9fd196382be3bdf
Josh Pencheon <josh.pencheon@googlemail.com>
2025-05-21 14:04:53 +0100

    nvim: update clipboard docs

--- .config/nvim/lua/init/clipboard.lua
+++ .config/nvim/lua/init/clipboard.lua
@@ -5,11 +5,11 @@
 -- Because we prefer the unnamedplus register, NeoVim will yank to
 -- and paste from one of these clipboard buffers by default.
 --
--- It is already the case that tmux will use OSC52 codes to tell iTerm
--- to try to update the client clipboard; we use a plugin to do something
--- similar in NeoVim - emitting OSC52 codes when yanking into the unnamed
--- register. This means things mostly 'just work' when accessing NeoVim
--- over SSH.
+-- It is already the case that tmux will use OSC52 codes to tell the
+-- terminal emulator to try to update the client clipboard; NeoVim has
+-- also had native since 0.10 - emitting OSC52 codes when yanking into
+-- the unnamed register. This means things mostly 'just work' when
+-- accessing NeoVim over SSH.
 
 -- Use system (+ selection) clipboard by default:
 vim.opt.clipboard:prepend { "unnamed", "unnamedplus" }
