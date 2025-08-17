418fbd045190da70840c69e7b09c5883e8bad36d
Josh Pencheon <josh.pencheon@googlemail.com>
2025-08-17 20:38:24 +0100

    nvim: tweak clipboard over SSH
    As of nvim 0.11, the 'osc52' clipboard provider is automatically enabled
    over SSH, as long as the unnamed registers haven't been configured to be
    used by default for yanking/pasting.
    
    This configuration continues to prefer the latter setup for local usage,
    but as terminal emulators will generally prefer to request confirmation
    before allowing paste via OSC52, requires explict usage of the clipboard
    provider to access the (client) clipboard when connected over SSH.

--- .config/nvim/lua/init/clipboard.lua
+++ .config/nvim/lua/init/clipboard.lua
@@ -1,18 +1,10 @@
--- A note on clipboards: NeoVim's default clipboard provider will
--- try and use `pbcopy` on macOS, but will eventually fall back to
--- tmux's buffer.
---
--- Because we prefer the unnamedplus register, NeoVim will yank to
--- and paste from one of these clipboard buffers by default.
---
--- It is already the case that tmux will use OSC52 codes to tell the
--- terminal emulator to try to update the client clipboard; NeoVim has
--- also had native since 0.10 - emitting OSC52 codes when yanking into
--- the unnamed register. This means things mostly 'just work' when
--- accessing NeoVim over SSH.
-
--- Use system (+ selection) clipboard by default:
-vim.opt.clipboard:prepend { "unnamed", "unnamedplus" }
+-- use system (+ selection) clipboard by default, unless
+-- running in an SSH session; in which case, nvim 0.11+
+-- will automatically enable the OSC52 clipboard provider
+-- for the unnamed registers.
+if not vim.env.SSH_TTY then
+  vim.opt.clipboard:prepend { "unnamed", "unnamedplus" }
+end
 
 vim.api.nvim_create_autocmd("TextYankPost", {
     pattern = "*",
