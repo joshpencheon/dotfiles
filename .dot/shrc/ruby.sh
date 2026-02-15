bdea0c914618e87c5f529ea1e8712fb470e63cbb
Josh Pencheon <josh.pencheon@googlemail.com>
2026-02-15 13:14:51 +0000

    rbenv: replaced with mise

--- .dot/shrc/ruby.sh
+++
@@ -1,9 +0,0 @@
-# rbenv
-export PATH="$HOME/.rbenv/bin:$PATH"
-if command -v rbenv > /dev/null; then
-  eval "$(rbenv init -)"
-fi
-
-# jRuby:
-export JRUBY_OPTS="--server -J-Xms1500m -J-Xmx1500m -J-XX:+UseConcMarkSweepGC -J-XX:-UseGCOverheadLimit -J-XX:+CMSClassUnloadingEnabled"
-
